aed3b94d86a9442b6b5fb3447aad2fd8346adbde
Tony Fortes Ramos <tony.fortesramos@ogd.nl>
2016-10-17 17:16:30 +0200

    Added comment based help and fixed some formatting issues.

--- Modules/OAuth2Helper/OAuth2Helper.psm1
+++ Modules/OAuth2Helper/OAuth2Helper.psm1
@@ -4,33 +4,25 @@
 function OAuth2OpenWindow {
     <#
         .SYNOPSIS
-            Brief description.
+            Opens a browser window to allow authentication.
         .DESCRIPTION
-            Longer description. 
-        .PARAMETER Name1
-            Information about a parameter.
-        .PARAMETER Name2
-            Information about a parameter.
+            Opens the specified URL in a browser window to facilitate OAuth2 authentication. Can be used for more than just Microsoft APIs.
+        .PARAMETER Url
+            The Url that should be opened.
         .EXAMPLE
-            Cmdlet
+            OAuth2OpenWindow -Url $requestUrl
             
             -----------
         
-            Explanation about the example.
+            Opens a browser window with the specified page.
         .INPUTS
-        	Information about inputs and piping
+        	This command does not accept pipeline input.
         .OUTPUTS
-        	Information abou the outputs.
-        .NOTES
-            Extra notes.
-        .LINK
-        	Related Cmdlets.
-        .COMPONENT
-            Used Cmdlets. 
+        	This command outputs the returned data.
     #>
     [CmdletBinding()]
     param(
-        [Parameter()]
+        [Parameter(Mandatory = $true)]
         $Url
     )
 
@@ -41,10 +33,11 @@ function OAuth2OpenWindow {
         #Load the System.Windows.Forms assembly required to open the WebPage to authenticate
         Add-Type -AssemblyName System.Windows.Forms
 
+        #Create a form and a webbrowser object to display the authentication page.
         $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width=440;Height=640}
         $web  = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width=420;Height=600;Url=($Url -f ($Scope -join "%20")) }
 
-        #Specify what to do when the browser notices that an action has been completed.
+        #Specify what the browser should do once a message matching the regex specified below has been returned.
         $docComp  = {
             $uri = $web.Url.AbsoluteUri        
             if ($uri -match "error=[^&]*|code=[^&]*") {$form.Close() }
@@ -69,42 +62,53 @@ function OAuth2OpenWindow {
 function Get-OAuth2AzureAuthorizationCode {
     <#
         .SYNOPSIS
-            Brief description.
+            Retrieves an Azure authorization code.
         .DESCRIPTION
-            Longer description. 
-        .PARAMETER Name1
-            Information about a parameter.
-        .PARAMETER Name2
-            Information about a parameter.
+            This cmdlet retrieves an Azure REST API authorization code by displaying a pop up browser window where you log in. 
+        .PARAMETER ClientId
+            The client/application ID that identifies this application.
+        .PARAMETER TenantId
+            The tenant ID that identifies your organization. Can be a GUID or one of your verified domain names.
+        .PARAMETER RedirectUri
+            The URI to where you should be redirected after authenticating. Native Apps should use 'urn:ietf:wg:oauth:2.0:oob' as their Redirect URI.
+        .PARAMETER ResourceUri
+            The URI of the resource you're trying to access.
+        .PARAMETER Scope
+            An array of the permissions you require from this application. Necessary for the V2.0 API. 
+        .PARAMETER AdminConsent
+            Specifies if administrative consent is necessary to run this application.
         .EXAMPLE
-            Cmdlet
+            Get-OAuth2AzureAuthorizationCode -ClientId $appId
             
             -----------
         
-            Explanation about the example.
+            Opens a browser window to login.microsoftonline.com so you can log in and retrieve an authorization code.
         .INPUTS
-        	Information about inputs and piping
+        	This command does not accept pipeline input.
         .OUTPUTS
-        	Information abou the outputs.
-        .NOTES
-            Extra notes.
+        	This command outputs the returned authorization code.
         .LINK
-        	Related Cmdlets.
+        	Get-OAuth2AzureAccessToken
         .COMPONENT
-            Used Cmdlets. 
+            OAuth2OpenWindow 
     #>
     [CmdletBinding()]
     param(
-        [Parameter()]
+        [Parameter(Mandatory = $true)]
         [string]$ClientId,
+
         [Parameter()]
         [string]$TenantId = 'common',
+
         [Parameter()]
         $RedirectUri = 'urn:ietf:wg:oauth:2.0:oob',
+
         [Parameter()]
         [string[]]$ResourceUri,
+
         [Parameter()]
         [string]$Scope,
+
         [Parameter()]
         [switch]$AdminConsent
     )
@@ -116,7 +120,7 @@ function Get-OAuth2AzureAuthorizationCode {
         #Load the System.Web assembly required to encode the values that will be entered into the URL.
         Add-Type -AssemblyName System.Web
         
-        # UrlEncode the ClientID and ClientSecret and URL's for special characters 
+        #UrlEncode the redirect URI, resource and scope for special characters 
         $redirectUriEncoded =  [System.Web.HttpUtility]::UrlEncode($redirectUri)
         $resourceEncoded = [System.Web.HttpUtility]::UrlEncode($resourceUri)
         $scopeEncoded = [System.Web.HttpUtility]::UrlEncode($Scope)
@@ -129,17 +133,16 @@ function Get-OAuth2AzureAuthorizationCode {
             { $ResourceUri }    { $url += "&resource=$resourceEncoded" }
             { $AdminConsent }   { $url += "&prompt=admin_consent" }
             { $Scope }          { $url += "&scope=$scopeEncoded" }
-
         }
-        # Get Authorization Code
+        #Open a window to the specific url and authenticate with your credentials.
         $query = OAuth2OpenWindow -Url $url
-
+        #Parse the query so the code and session state can be found.
         $queryOutput = [System.Web.HttpUtility]::ParseQueryString($query.Url.Query)
         $output = @{}
         foreach($key in $queryOutput.Keys){
             $output["$key"] = $queryOutput[$key]
         }
-        # Extract Access token from the returned URI
+        #Combine the code and session state to form the authorization code.
         $authorizationCode = "$($output.code)&session_state=$($output.session_state)"
     }
     
@@ -152,56 +155,67 @@ function Get-OAuth2AzureAuthorizationCode {
 function Get-OAuth2AzureAccessToken {
     <#
         .SYNOPSIS
-            Brief description.
+            Retrieves an access token.
         .DESCRIPTION
-            Longer description. 
-        .PARAMETER Name1
-            Information about a parameter.
-        .PARAMETER Name2
-            Information about a parameter.
+            Uses the authorization code to request an access token for a specific resource. 
+        .PARAMETER ClientId
+            The client/application ID that identifies this application.
+        .PARAMETER ClientSecret
+            The secret key used to authenticate your application.
+        .PARAMETER TenantId
+            The tenant ID that identifies your organization. Can be a GUID or one of your verified domain names.
+        .PARAMETER RedirectUri
+            The URI to where you should be redirected after authenticating. Native Apps should use 'urn:ietf:wg:oauth:2.0:oob' as their Redirect URI.
+        .PARAMETER ResourceUri
+            The URI of the resource you're trying to access.
+        .PARAMETER AuthorizationCode
+            The authorization code necessary to request an access token.
         .EXAMPLE
-            Cmdlet
+            Get-OAuth2AzureAccessToken -ClientId $appId -ClientSecret $key -ResourceUri https://graph.microsoft.com -AuthorizationCode $authCode
             
             -----------
         
-            Explanation about the example.
+            Returns an access token for use in API requests.
         .INPUTS
-        	Information about inputs and piping
+        	This command does not accept pipeline input.
         .OUTPUTS
-        	Information abou the outputs.
-        .NOTES
-            Extra notes.
+        	This command outputs the returned access token.
         .LINK
-        	Related Cmdlets.
+        	Get-OAuth2AzureAuthorizationCode
         .COMPONENT
-            Used Cmdlets. 
+            OAuth2OpenWindow 
     #>
     [CmdletBinding()]
     param(
-        [Parameter()]
+        [Parameter(Mandatory = $true)]
         [string]$ClientId,
-        [Parameter()]
+
+        [Parameter(Mandatory = $true)]
         [string]$ClientSecret,
+
         [Parameter()]
         [string]$TenantId = 'common',
+
         [Parameter()]
         $RedirectUri = 'urn:ietf:wg:oauth:2.0:oob',
-        [Parameter()]
-        [string]$AuthorizationCode,
-        [Parameter()]
-        [string[]]$ResourceUri
+
+        [Parameter(Mandatory = $true)]
+        [string[]]$ResourceUri,
+
+        [Parameter(Mandatory = $true)]
+        [string]$AuthorizationCode
     )
 
     begin {    
     }
 
     process {
+        #Load the System.Web assembly required to encode the values that will be entered into the URL.
         Add-Type -AssemblyName System.Web
         
-        # UrlEncode the ClientID and ClientSecret and URL's for special characters 
+        #UrlEncode the ClientSecret for special characters.
         $clientSecretEncoded = [System.Web.HttpUtility]::UrlEncode($ClientSecret)
-
-        #get Access Token
+        #prepare the parameters that should be passed to the Invoke-Restmethod cmdlet.
         $body = "grant_type=authorization_code&redirect_uri=$RedirectUri&client_id=$ClientId&client_secret=$clientSecretEncoded&code=$AuthorizationCode&resource=$ResourceUri"
         $invokeRestMethodParams = @{
             Uri = "$Script:authenticationUrl/common/oauth2/token"
@@ -210,7 +224,9 @@ function Get-OAuth2AzureAccessToken {
             Body = $body
             ErrorAction = 'Stop'    
         }
+        #Perform the request
         $authorization = Invoke-RestMethod @invokeRestMethodParams
+        #Get the access token
         $accessToken = $authorization.access_token
     }
 
