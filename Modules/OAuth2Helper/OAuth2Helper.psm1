7ba08c8fbce9ce9fe1cdf6983568c770288aff5e
tony.fortesramos <tony.fortesramos@ogd.nl>
2016-10-19 00:32:43 +0200

    Fixed scopes in V2, improved help

--- Modules/OAuth2Helper/OAuth2Helper.psm1
+++ Modules/OAuth2Helper/OAuth2Helper.psm1
@@ -68,27 +68,37 @@ function Get-OAuth2AzureAuthorization {
         .PARAMETER ClientId
             The client/application ID that identifies this application.
         .PARAMETER TenantId
-            The tenant ID that identifies your organization. Can be 'common' or your 'tenant ID'. If version 2.0 of the API is used, 'consumer' and 'organization' can be specified as well.
+            The tenant ID that identifies the organization. Can be 'common' or a specific tenant ID. If version 2.0 of the API is used, 'consumer' and 'organization' can be specified as well. Consumer specifies that only Microsoft Accounts can authenticate. Organization allows only Organizational Accounts to log in.
         .PARAMETER RedirectUri
-            The URI to where you should be redirected after authenticating. Native apps should use 'urn:ietf:wg:oauth:2.0:oob' as their Redirect URI in version 1.0. In version 2.0 'https://login.microsoftonline.com/common/oauth2/nativeclient' should be specified for native apps.
+            The URI to where you should be redirected after authenticating. Native apps should use 'urn:ietf:wg:oauth:2.0:oob' as their Redirect URI in version 1.0. In version 2.0 'https://login.microsoftonline.com/common/oauth2/nativeclient' should be specified for native apps, however 'urn:ietf:wg:oauth:2.0:oob' also works.
         .PARAMETER Scope
             An array of the permissions you require from this application. Required when using the v2.0 API.
+            In version 1.0 specify the scopes as 'calendars.read' or 'user.readwrite'.
+            When using version 2.0, specify the scopes in the format 'http://graph.microsoft.com/user.readbasic.all' and 'https://outlook.office.com/mail.read'.
         .PARAMETER Prompt
             Specifies what type of login is needed.None specifies single sign-on. Login specifies that credentials must be entered and SSO is negated. Consent specifies that the user must give consent. Not available with the v2.0 authentication API, Admin_Consent specifies that an admin automatically approves the application for all users.
         .PARAMETER ApiV2
             Enables the use of version 2.0 of the authentication API. Version 2.0 apps can be registered at https://apps.dev.microsoft.com/.
         .EXAMPLE
-            Get-OAuth2AzureAuthorizationCode -ClientId $appId
+            Get-OAuth2AzureAuthorization -ClientId $appId -TenantId contoso.com
         
             Code         : O2tTBPNzSgjnjaZWCoBial92z4c6QpoOzM-M8qy16_IGif6NQz-TGF_Z3AenDL1fffUB5JyBHpB0mKylnDIdikaibRIuiWfUdH...
             SessionState : fed8744b-c5cf-4935-b836-142756485e48
             State        : 031d3567-25c3-123f-a4d4-8a7e7fb2343e
 
-            Opens a browser window to login.microsoftonline.com and retrieve an authorization code.
+            Opens a browser window to login.microsoftonline.com and retrieve an authorization code using version 1.0 of the API.
+        .EXAMPLE
+            Get-OAuth2AzureAuthorization -ClientId $apiv2ClientId -Scope $Scope -Prompt Consent -RedirectUri 'https://login.microsoftonline.com/common/oauth2/nativeclient' -ApiV2
+        
+            Code         : GYkA6Ses3jm62gaJTFrt0tlrPBMMPWBM_BXG2hciutILnTAMGOReRfZZ3OXBNqLDl5tD24dTeMosol9eIVlTXXfAkGekWWgkci...
+            SessionState : 9c4b9ec2-3dd9-4762-939a-e0bf877a4ac4
+            State        : a52c08af-8b94-434e-878e-793f4e66a62b
+
+            Opens a browser window to login.microsoftonline.com and retrieve a version 2.0 authorization code. The aurthorization code grants only the access specified in the scope.
         .INPUTS
         	This command does not accept pipeline input.
         .OUTPUTS
-        	This command outputs the returned authorization code.
+        	This command outputs the returned authorization code, state and session state.
         .LINK
         	Get-OAuth2AzureToken
         .COMPONENT
@@ -105,8 +115,9 @@ function Get-OAuth2AzureAuthorization {
         [Parameter()]
         [string]$RedirectUri = 'urn:ietf:wg:oauth:2.0:oob',
 
-        [Parameter()]
-        [string]$Scope,
+        [Parameter(ParameterSetName = 'ApiV2', Mandatory = $true)]
+        [Parameter(ParameterSetName = 'None')]
+        [string[]]$Scope,
 
         [Parameter()]
         [ValidateSet('Login','Consent','Admin_Consent','None')]
@@ -172,29 +183,39 @@ function Get-OAuth2AzureToken {
         .PARAMETER ClientSecret
             The secret key used to authenticate your application. In version 2.0 of the API it cannot be used for native apps.
         .PARAMETER TenantId
-            The tenant ID that identifies your organization. Can be 'common' or your 'tenant ID'. If version 2.0 of the API is used, 'consumer' and 'organization' can be specified as well.
+            The tenant ID that identifies the organization. Can be 'common' or a specific tenant ID. If version 2.0 of the API is used, 'consumer' and 'organization' can be specified as well. Consumer specifies that only Microsoft Accounts can authenticate. Organization allows only Organizational Accounts to log in.
         .PARAMETER RedirectUri
-            The URI to where you should be redirected after authenticating. Native apps should use 'urn:ietf:wg:oauth:2.0:oob' as their Redirect URI in version 1.0. In version 2.0 'https://login.microsoftonline.com/common/oauth2/nativeclient' should be specified for native apps.
+            The URI to where you should be redirected after authenticating. Native apps should use 'urn:ietf:wg:oauth:2.0:oob' as their Redirect URI in version 1.0. In version 2.0 'https://login.microsoftonline.com/common/oauth2/nativeclient' should be specified for native apps, however 'urn:ietf:wg:oauth:2.0:oob' also works.
         .PARAMETER ResourceUri
-            The URI of the resource you're trying to access. Only required for version 1.0 of the API.
+            The URI of the resource you're trying to access. Only used by version 1.0 of the API. Specify this parameter in the format 'https://outlook.office.com'.
         .PARAMETER Scope
-            An array of the permissions you require from this application. Can only be the same or a superset of the scope defined in the authorization request.
+            An array of the permissions you require from this application. Can only be the same or a superset of the scope defined in the authorization request. Mandatory for version 2.0 of the API.
+            In version 1.0 specify the scopes as 'calendars.read' or 'user.readwrite'.
+            When using version 2.0, specify the scopes in the format 'http://graph.microsoft.com/user.readbasic.all' and 'https://outlook.office.com/mail.read'.
         .PARAMETER AuthorizationCode
-            The authorization code necessary to request an access token.
+            The authorization code necessary to request an access token. Can be attained by using Get-OAuth2AzureAuthorization.
         .PARAMETER ApiV2
             Enables the use of version 2.0 of the authentication API. Version 2.0 apps can be registered at https://apps.dev.microsoft.com/.
         .EXAMPLE
-            Get-OAuth2AzureAccessToken -ClientId $appId -ClientSecret $key -ResourceUri https://graph.microsoft.com -AuthorizationCode $authCode
+            Get-OAuth2AzureAccessToken -ClientId $appId -ClientSecret $key -RedirectUri https://app.fabrikam.com/endpoint -ResourceUri https://graph.microsoft.com -AuthorizationCode $authCode
             
             RefreshToken : JTMjU2IiwieDV0IjoiSTZvQnc0VnpCSE9xbGVHclYyQUpkQTVFbVhjIiwia2lkIjoiSTZvQnc0VnpCSE9xbGVHclYyQUpkQTVF...
             AccessToken  : eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFEUk5ZUlEzZGhSU3JtLTRLLWFkcENKYUVOeFFJUHlXLVRieUVwWllzSG...
             IdToken      : uUmVhZCIsInN1YiI6IjByMEJyTl9GOGxRck5aeHJBS0RFNHhTQzFzbWJIRDRvOXU0dkVHTG9Kb00iLCJ0aWQiOiJlZmQzYzc2Y...
 
-            Returns an access token for use in API requests.
+            Returns an access token, refresh token and ID token for use in API requests.
+        .EXAMPLE
+            Get-OAuth2AzureAccessToken -ClientId $appId -Scope 'http://graph.microsoft.com/user.readbasic.all' -AuthorizationCode $authCode -ApiV2
+            
+            RefreshToken : 
+            AccessToken  : AQABAAAAAADRNYRQ3dhRSrmU7elRk-QCim_YGkzAatTUt0ESeVdvfpNlx6P-ec7mO9HiVu2BJ6Z0RbCagK4tCEPK1g0yNf3i-P...
+            IdToken      : 
+
+            Returns an access token for use in API requests. As security cannot be guaranteed on native apps, the 2.0 version does not return refresh and ID tokens.
         .INPUTS
         	This command does not accept pipeline input.
         .OUTPUTS
-        	This command outputs the returned access token.
+        	This command outputs the returned access token, refresh token and ID token.
         .LINK
         	Get-OAuth2AzureAuthorization
         .COMPONENT
@@ -218,8 +239,9 @@ function Get-OAuth2AzureToken {
         [Parameter(ParameterSetName = 'ApiV1', Mandatory = $true)]
         [string]$ResourceUri,
 
-        [Parameter()]
-        [string]$Scope,
+        [Parameter(ParameterSetName = 'ApiV1')]
+        [Parameter(ParameterSetName = 'ApiV2', Mandatory = $true)]
+        [string[]]$Scope,
 
         [Parameter(Mandatory = $true)]
         [string]$AuthorizationCode,
